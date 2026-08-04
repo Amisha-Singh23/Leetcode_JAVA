ce13b74537d23a50ebeceb38a2d5d831ffff00d9
Amisha-Singh23 <amishauttaranchal@gmail.com>
2026-08-04 11:58:09 +0530

    Time: 0 ms (100%), Space: 8.4 MB (98.36%) - LeetHub

---
+++ 0007-reverse-integer/0007-reverse-integer.cpp
@@ -0,0 +1,32 @@
+#include <iostream>
+using namespace std;
+class Solution {
+public:
+    int reverse(int x) {
+        int rev = 0;
+
+        while (x != 0) {
+            int digit = x % 10;
+            x = x / 10;
+
+            if (rev > INT_MAX / 10 || rev < INT_MIN / 10) {
+                return 0;
+            }
+
+            rev = rev * 10 + digit;
+        }
+
+        return rev;
+    }
+
+    int main() {
+        int x;
+        cout << "Enter an integer: ";
+        cin >> x;
+
+        int result = reverse(x);
+        cout << "Reversed integer: " << result << endl;
+
+        return 0;
+    }
+};
