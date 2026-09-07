4f70b5547fb4eb138eb176278af49aac808a3090
Amisha-Singh23 <amishauttaranchal@gmail.com>
2026-09-07 19:35:41 +0530

    Time: 1 ms (99.8%), Space: 42.6 MB (98.16%) - LeetHub

---
+++ 0389-find-the-difference/0389-find-the-difference.java
@@ -0,0 +1,13 @@
+class Solution {
+    public char findTheDifference(String s, String t) {
+
+        char ans = 0;
+        for(char ch: s.toCharArray()){
+            ans =(char)(ans ^ch);
+           }
+        for(char ch: t.toCharArray()){
+            ans = (char)(ans ^ch);
+        }
+        return ans;
+    }
+}
